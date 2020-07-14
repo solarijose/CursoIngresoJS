861b623a323d7272f975dea52c28a13d749d1537
Solari Jose Luis <solarijose@gmail.com>
2020-07-14 15:42:50 -0300

    master

--- 1-EntradaSalida/jsEntradaSalida-03.js
+++ 1-EntradaSalida/jsEntradaSalida-03.js
@@ -3,9 +3,13 @@ Debemos lograr tomar un dato por 'ID'
 y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	var nombreIngresado;
-	nombreIngresado=txtIdNombre.value;
-	alert("ok");
+	var dato;
+	
+	//dato = dato por ID.Value
+
+	dato = prompt ("Ingrese su nombre")
+
+	txtIdNombre.value = dato;
 
 }
 
