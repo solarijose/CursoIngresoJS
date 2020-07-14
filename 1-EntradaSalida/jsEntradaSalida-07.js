9abff6bb08dc25f2c1dd2852499f15595bebc346
Solari Jose Luis <solarijose@gmail.com>
2020-07-14 19:43:01 -0300

    maser

--- 1-EntradaSalida/jsEntradaSalida-07.js
+++ 1-EntradaSalida/jsEntradaSalida-07.js
@@ -5,21 +5,51 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	alert("ok");	
+	var sumar;
+	var NumeroUno;
+	var NumeroDos;
+
+	numeroUno = parseInt(numeroUno);
+	numeroDos = parseInt(numeroDos);
+
+	suma = numeroUno + numeroDos;
+
+	alert("El resultado es " + sumar);	
 }
 
 function restar()
 {
-	
+	var resta;
+	var NumeroUno;
+	var NumeroDos;
+
+	numeroUno = parseInt(numeroUno);
+	numeroDos = parseInt(numeroDos);
+
+	alert("la resta es"+ restar);
 }
 
 function multiplicar()
 { 
-	
+	var multiplicacion;
+	var NumeroUno;
+	var NumeroDos;
+
+	numeroUno = parseInt(numeroUno);
+	numeroDos = parseInt(numeroDos);
+
+	alert("El resultado de la mutiplicacion es "+ multiplicar);
 }
 
 function dividir()
 {
-	
+	var division;
+	var NumeroUno;
+	var NumeroDos;
+
+	numeroUno = parseInt(numeroUno);
+	numeroDos = parseInt(numeroDos);
+
+	alert("la division es "+ division);	
 }
 
