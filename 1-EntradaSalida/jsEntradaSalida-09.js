1e93b7536577ff6b2de8c9f87dde617772851156
Solari Jose Luis <solarijose@gmail.com>
2020-07-15 02:11:28 -0300

    master

--- 1-EntradaSalida/jsEntradaSalida-09.js
+++ 1-EntradaSalida/jsEntradaSalida-09.js
@@ -3,19 +3,25 @@ Debemos lograr tomar el importe por ID ,
 transformarlo a entero (parseInt), luego
 mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
+
+
 function mostrarAumento()
 {
+	//Variables
+	var sueldo;
+	var resultado;
+	
+	// sueldo Debemos lograr tomar el importe por ID,transformarlo a entero (parseInt)
+	sueldo = txtIdSueldo.value;
+	sueldo = parseInt(sueldo)
+	
+	// resultado 
+	resultado = sueldo + sueldo * 10/100; //
 
-	var Sueldo;
-	var Resultado;
-	var Aumento;
-
-	Sueldo = txtIdSueldo.value;
-
-	Sueldo = parseInt(Sueldo)
-
-	txtIdResultado
-
-
-	alert("Mostrar aumento "+ Aumento );
+	//luego mostrar el importe con un aumento del 10 %
+	alert("Mostrar aumento "+ resultado );
+	txtIdResultado.value = resultado; 
+	
+	// Recordar ingresar primero ejemplo.value = variable para ingresar en cuadro RESULTADO
+	
 }
