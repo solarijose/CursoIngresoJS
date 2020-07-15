9356dceb65ecac2b6d28d4dbceb5d6f92ecc0db9
Solari Jose Luis <solarijose@gmail.com>
2020-07-15 02:27:22 -0300

    master

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -5,5 +5,22 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	alert("ok");
+	//Variables
+	var importe;
+	var resultado;
+
+	// Importes
+	importe = txtIdImporte.value;
+	importe = parseInt(importe);
+
+	//Resuldado con Descuento
+	resultado = importe - 25*100;
+
+	alert("Importe total con descuento "+ resultado);
+	txtIdResultado.value = resultado;
+
+
+
+
+
 }
