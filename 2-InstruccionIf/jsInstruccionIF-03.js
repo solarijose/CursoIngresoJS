781ced2ebaa32a5e1980a5b4a721332f5b1fb940
Solari Jose Luis <solarijose@gmail.com>
2020-07-15 22:16:02 -0300

    master

--- 2-InstruccionIf/jsInstruccionIF-03.js
+++ 2-InstruccionIf/jsInstruccionIF-03.js
@@ -1,6 +1,20 @@
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	/*Al ingresar una edad debemos informar si la persona es mayor de edad,
+	sino informar que es un menor de edad.
+	tomo la edad */ 
 
+	var edad;
+
+	edad = txtIdEdad.value;
+	edad = parseInt(edad);
+	
+	if (edad>17)
+	alert("La persona es mayor de edad");
+
+	{
+	else (edad<17)
+	alert("La persona es menor de edad");
+
+	}
 }//FIN DE LA FUNCIÓN
