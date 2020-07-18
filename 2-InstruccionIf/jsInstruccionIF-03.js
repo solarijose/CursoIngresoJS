99ab0e55a49a39e544f76936febed4f9875c65ce
Solari Jose Luis <solarijose@gmail.com>
2020-07-18 01:45:32 -0300

    TP

--- 2-InstruccionIf/jsInstruccionIF-03.js
+++ 2-InstruccionIf/jsInstruccionIF-03.js
@@ -10,11 +10,9 @@ function mostrar()
 	edad = parseInt(edad);
 	
 	if (edad>17)
-	alert("La persona es mayor de edad");
-
-	{
-	else (edad<17)
+	alert("La persona es mayor de edad");	
+	
+		else if (edad<17)
 	alert("La persona es menor de edad");
 
-	}
 }//FIN DE LA FUNCIÓN
