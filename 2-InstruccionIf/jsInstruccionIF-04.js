f1ae89d5662c4488d3fc137656eaeb604e2668d5
Solari Jose Luis <solarijose@gmail.com>
2020-07-19 17:07:44 -0300

    TP

--- 2-InstruccionIf/jsInstruccionIF-04.js
+++ 2-InstruccionIf/jsInstruccionIF-04.js
@@ -1,7 +1,25 @@
+
+//Al ingresar una edad debemos informar si la persona es adolescente, edad entre 13 y 17 años (inclusive).
+
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	//declaro variable
+	var edad;
 
+	//declaro funcion
+	edad = txtIdEdad.value;
+	edad = parseInt(edad);
 
+	// condicion IF
+	if(edad > 12)
+
+	{
+		if(edad < 18)
+
+		{
+			alert("Usted es adolecente")
+		}
+
+	}
+	
 }//FIN DE LA FUNCIÓN
