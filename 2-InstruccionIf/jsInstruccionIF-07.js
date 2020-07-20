c3ac793d9fb3ad1eb33618e0ec3d916ab4dc60a0
Solari Jose Luis <solarijose@gmail.com>
2020-07-20 02:11:54 -0300

    TP

--- 2-InstruccionIf/jsInstruccionIF-07.js
+++ 2-InstruccionIf/jsInstruccionIF-07.js
@@ -1,7 +1,24 @@
+/*Al ingresar una edad menor a 18 años y un estado civil distinto a
+ "Soltero", mostrar el siguiente mensaje: 'Es muy pequeño para NO ser soltero.'
+*/
+
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	var edad;
+	var EstadoCivil;
+
+	edad = txtIdEdad.value;
+	edad = parseInt(edad);
+
+	estadoCivil = estadoCivil.value;
+	estadoCivil = parseInt(estadoCivil);
+
+	if (edad <18 && EstadoCivil != "Soltero")
+	{
+		alert("Es muy pequeño para NO ser soltero");
+	}
+
+
 	
 
 
