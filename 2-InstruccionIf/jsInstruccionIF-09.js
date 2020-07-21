358c1f98e3adabb62a1eb977c19b008dc32d971f
Solari Jose Luis <solarijose@gmail.com>
2020-07-20 22:33:03 -0300

    TP

--- 2-InstruccionIf/jsInstruccionIF-09.js
+++ 2-InstruccionIf/jsInstruccionIF-09.js
@@ -2,9 +2,9 @@ function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
 	
-	var aleatorio = (Math.random()* 10);
+	var aleatorio = Math.floor(Math.random() * 10);
 
-	alert("Genero un numero random entre 1 y 10 " + aleatorio);
+	alert(aleatorio);
 
 
 }//FIN DE LA FUNCIÓN
