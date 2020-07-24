7f86be4280ce0c635c5eac1170150fd2f3ea85c1
Solari Jose Luis <solarijose@gmail.com>
2020-07-24 17:11:40 -0300

    TP

--- 8-TPs/04-jsFerreteIluminacion.js
+++ 8-TPs/04-jsFerreteIluminacion.js
@@ -17,100 +17,81 @@ E. Si el importe final con descuento suma más de $120 se debe sumar un 10%
 
 function CalcularPrecio()
 {
- 
-    var precioLamparas;
+   //declaracion y asignacion de variables
+    var precioLamparas = 35;
     var cantidadLamparas;
     var marcaLamparas;
     var descuento;
     var precioTotal;
     var precioDescuento;
-    var impuestos;
-    var costoImpuestos;
+    var impuestos = 0.1;
 
-
-    precioLamparas = 35;
-    impuestos = 0,1;
-    costoImpuestos = 0;
-    
+    //asignacion de variables
     cantidadLamparas = txtIdCantidad.value;
     cantidadLamparas = parseInt(cantidadLamparas);
     marcaLamparas = Marca.value;
+    // /*
+    // //A. Si compra 6 o más lamparitas bajo consumo tiene un descuento del 50%.
     
-
-
-    precioTotal = cantidadLamparas * precioLamparas;
-    descuento = descuento * cantidadLamparas;
-
-    //A. Si compra 6 o más lamparitas bajo consumo tiene un descuento del 50%.
-    if (cantidadLamparas >5 == "ArgentinaLuz" + descuento)
+    if (cantidadLamparas>5)
     {
-        precioLamparas;
-        descuento = 0,5;
-        precioTotal;
-    }
-    else (cantidadLamparas >3)
-    {
-        precioLamparas;
-        precioTotal; 
-    }
-    alert(precioTotal)
-    
- /*B. Si compra 5 lamparitas bajo consumo marca "ArgentinaLuz"
-  se hace un descuento del 40 %
-  y si es de otra marca el descuento es del 30%.*/
-    if (cantidadLamparas == 5 == "ArgentinaLuz")
-    {
-        descuento = 0,5;
-    }
-
-    else (cantidadLamparas == 5)
-    
-    {
-        descuento = 0,3;
-    }
-
-  /*C. Si compra 4 lamparitas bajo consumo marca "ArgentinaLuz" o “FelipeLamparas”
-  se hace un descuento del 25 % y si es de otra marca el descuento es del 20%.*/
-    
-  if (cantidadLamparas == 4 + "ArgentinaLuz" || cantidadLamparas == 4 + "FelipeLamparas")
-    {
-        descuento = 0,25;
-    }
-
-    else (cantidadLamparas == 4)
- 
-    {
-        descuento=0,20;
-    }
-
-    /*D.compra 3 lamparitas"ArgentinaLuz" descuento del 15%,
-    si es “FelipeLamparas” se hace un descuento del 10 % 
-    y si es de otra marca un 5%.*/ 
-    if (cantidadLamparas == 3 == "ArgentinaLuz" || cantidadLamparas == 3 == "FelipeLamparas")
-    {
-        descuento = 0,15;
-    }
+        descuento=0.5;
+    }    
+    // B. Si compra 5 lamparitas bajo consumo marca "ArgentinaLuz"
+    // se hace un descuento del 40 %
+    // y si es de otra marca el descuento es del 30%.
     else
     {
-        descuento = 0,10;
-    }
-    if ( cantidadLamparas>2)
-    {
-        descuento = 0,05;
-    }
+        if (cantidadLamparas==5) //&& operador Y = separacion condicion.
+        {
+            if (marcaLamparas == "ArgentinaLuz") 
+            {
+                descuento = 0.4;
+            }
+            else 
+            {
+                descuento = 0.3;
+            }
+            alert(descuento);
+            // C. Si compra 4 lamparitas bajo consumo marca "ArgentinaLuz" o “FelipeLamparas”
+            // se hace un descuento del 25 % y si es de otra marca el descuento es del 20%.
+            
+        }
+        else 
+        {
+            if (cantidadLamparas==4)
+            {
+                if(marcaLamparas == "ArgentinaLuz" || marcaLamparas == "FelipeLamparas")
+                {
+                    descuento=0.25;
+                }
+                else
+                {
+                    descuento=0.20;
+                }
+                
+            }
+            else 
+            {
+                // D.compra 3 lamparitas"ArgentinaLuz" descuento del 15%,
+                // si es “FelipeLamparas” se hace un descuento del 10 % 
+                // y si es de otra marca un 5%.
+                if()
+            }
+        }
+          
 
-    /*E. Si el importe final con descuento suma más de $120 se debe sumar un 10%
-    de ingresos brutos en informar del impuesto con el siguiente mensaje: 
-    ”IIBB Usted pago X”, siendo X el impuesto que se pagó*/   
-
-    precioDescuento = precioTotal * descuento;
- 
-    if (precioDescuento >120)
-    {  
-        costoImpuestos = precioDescuento * impuestos;
-        alert("Usted pago "+ impuestos + "de IIBB")
     }
-
     
+ 
+   
+
+    // E. Si el importe final con descuento suma más de $120 se debe sumar un 10%
+    // de ingresos brutos en informar del impuesto con el siguiente mensaje: 
+    // ”IIBB Usted pago X”, siendo X el impuesto que se pagó
+
+
+
+    // */   
 
 }
