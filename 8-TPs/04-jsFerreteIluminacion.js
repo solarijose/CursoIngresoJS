1c4470850eedba7025cf6fbfa130784eabc41c4d
Solari Jose Luis <solarijose@gmail.com>
2020-07-26 01:39:30 -0300

    TP

--- 8-TPs/04-jsFerreteIluminacion.js
+++ 8-TPs/04-jsFerreteIluminacion.js
@@ -19,125 +19,96 @@ function CalcularPrecio()
 {
 
     // declaracion y asignacion de variables
-
-    var precioLampras;
     var cantidadLamparas;
-    var marcaLamparas;
-    var importeFinal;
-    var precioDescuento;
-    var impuestos;
+    var precioLampras;
     var descuento;
+    var precioDescuento;
+    var importeFinal;
+    var marcaLamparas;
+    var impuestos;
 
     cantidadLamparas = txtIdCantidad.value;
     cantidadLamparas = parseInt(cantidadLamparas);
-    
     marcaLamparas = Marca.value;
-    precioLampras=35;
+    precioLampras= 35;
 
     /*A- Si compra 6 o más lamparitas bajo consumo 
     tiene un descuento del 50%.*/
 
-    if (cantidadLamparas >=6)
+    if (cantidadLamparas >5)
     {
-        cantidadLamparas= cantidadLamparas * precioLampras;
-        precioDescuento= cantidadLamparas * 50/100;
-        importeFinal = cantidadLamparas - precioDescuento;
-        
+    cantidadLamparas = cantidadLamparas * precioLampras;
+    precioDescuento = cantidadLamparas * 0.5;
+    importeFinal = cantidadLamparas - precioDescuento;
     }
     /*B. Si compra 5 lamparitas bajo consumo marca "ArgentinaLuz" 
     se hace un descuento del 40 % 
     y si es de otra marca el descuento es del 30%.*/
+    if(cantidadLamparas == 5 && marcaLamparas != "ArgentinaLuz")
+    {
+    cantidadLamparas = cantidadLamparas * precioLampras;
+    precioDescuento = cantidadLamparas *0.4;
+    importeFinal = cantidadLamparas - precioDescuento;
+    }
+    else
+    {
+    cantidadLamparas = cantidadLamparas * precioLampras;
+    precioDescuento = cantidadLamparas * 0.3;
+    importeFinal = cantidadLamparas - precioDescuento;
+    }
+    /*C. Si compra 4 lamparitas bajo consumo marca "ArgentinaLuz" 
+    o “FelipeLamparas” se hace un descuento del 25 % 
+    y si es de otra marca el descuento es del 20%.*/
+    if (cantidadLamparas == 4 && marcaLamparas == "ArgentinaLuz" || marcaLamparas == "FelipeLamparas")
+    {
+    cantidadLamparas = cantidadLamparas * precioLampras;
+    precioDescuento = cantidadLamparas * 0.25;
+    importeFinal = cantidadLamparas - precioDescuento;
+    }
     else 
     {
-        if (cantidadLamparas==5)
-        {
-            if (marcaLamparas == "ArgentinaLuz")
-            {
-                cantidadLamparas= cantidadLamparas * precioLampras;
-                precioDescuento= cantidadLamparas * 40/100;
-                importeFinal = precioDescuento - cantidadLamparas;
-            }
-            else
-            {
-                cantidadLamparas= cantidadLamparas * precioLampras;
-                precioDescuento= cantidadLamparas * 30/100;
-                importeFinal= cantidadLamparas - precioDescuento;
-            }
-        }
-        /*C. Si compra 4 lamparitas bajo consumo marca "ArgentinaLuz" 
-        o “FelipeLamparas” se hace un descuento del 25 % 
-        y si es de otra marca el descuento es del 20%.*/
-        else
-        {
-            if (cantidadLamparas == 4 )
-            {
-                if (marcaLamparas == "ArgentinaLuz" || marcaLamparas == "FelipeLamparas")
-                {
-                    cantidadLamparas= cantidadLamparas * precioLampras;
-                    precioDescuento= cantidadLamparas * 25/100;
-                    importeFinal= cantidadLamparas - precioDescuento;
-                }
-                else
-                {
-                    cantidadLamparas= cantidadLamparas * precioLampras;
-                    precioDescuento= cantidadLamparas * 20/100
-                    importeFinal= cantidadLamparas -precioDescuento;
-                }
-            }
-
-             
-             /*D. Si compra 3 lamparitas bajo consumo marca "ArgentinaLuz" 
-             el descuento es del 15%, si es “FelipeLamparas”
-             se hace un descuento del 10 % y si es de otra marca un 5%.*/
-            
-                else
-                {
-                if(cantidadLamparas == 3 && marcaLamparas == "ArgentinaLuz")
-                {
-                    cantidadLamparas= cantidadLamparas * precioLampras;
-                    precioDescuento= cantidadLamparas *15/100;
-                    importeFinal= cantidadLamparas - precioDescuento;
-                    
-                }
-                else if (cantidadLamparas == 3 && marcaLamparas == "FelipeLamparas")
-                {
-                    cantidadLamparas= cantidadLamparas * precioLampras;
-                    precioDescuento= cantidadLamparas * 10/100;
-                    importeFinal= cantidadLamparas - precioDescuento;
-                    
-                } 
-                else
-                {
-                    cantidadLamparas= cantidadLamparas * precioLampras;
-                    precioDescuento= cantidadLamparas *5/100;
-                    importeFinal= cantidadLamparas - precioDescuento;
-                    
-                }
-                    /*E. Si el importe final con descuento suma más de $120 se debe sumar un 10%
-                     de ingresos brutos en informar del impuesto con el siguiente mensaje: 
-                     ”IIBB Usted pago X”, siendo X el impuesto que se pagó*/
-
-                     if(importeFinal>120)
-                    {
-                        impuestos= importeFinal *10/100;
-                        importeFinal= importeFinal + impuestos;
-                        alert("IIBB Usted pago "+ impuestos + " de impuesto.");
-                    }
-                    descuento=txtIdprecioDescuento.value =importeFinal;
-                    descuento=parseInt(descuento);
-
-                
-            }
-        }
-
-
+    cantidadLamparas = cantidadLamparas * precioLampras;
+    precioDescuento = cantidadLamparas * 0.2;
+    importeFinal = cantidadLamparas - precioDescuento;
     }
-
-
-
-
-
-
+    /*D. Si compra 3 lamparitas bajo consumo marca "ArgentinaLuz" 
+    el descuento es del 15%, si es “FelipeLamparas”
+    se hace un descuento del 10 % y si es de otra marca un 5%.*/
+    if(cantidadLamparas == 3 && marcaLamparas == "ArgentinaLuz")
+    {
+    cantidadLamparas = cantidadLamparas * precioLampras;
+    precioDescuento = cantidadLamparas *0.15;
+    importeFinal = cantidadLamparas - precioDescuento;
+    }
+    if (cantidadLamparas == 3 && marcaLamparas == "FelipeLamparas")
+    {
+    cantidadLamparas = cantidadLamparas * precioLampras;
+    precioDescuento = cantidadLamparas * 0.1;
+    importeFinal = cantidadLamparas - precioDescuento;
+    } 
+    else
+    {
+    cantidadLamparas = cantidadLamparas * precioLampras;
+    precioDescuento = cantidadLamparas *0.5;
+    importeFinal = cantidadLamparas - precioDescuento;
+    }
+    /*E. Si el importe final con descuento suma más de $120 se debe sumar un 10%
+    de ingresos brutos en informar del impuesto con el siguiente mensaje: 
+    ”IIBB Usted pago X”, siendo X el impuesto que se pagó*/
+    if(importeFinal>120)
+    {
+    impuestos = importeFinal * 0.1;
+    importeFinal = importeFinal + impuestos;
+    alert("IIBB Usted pago "+ impuestos + " de impuesto.");
+    }
+    descuento=txtIdprecioDescuento.value = importeFinal;            
 }
+
+
+
+
+
+
+
 
 
