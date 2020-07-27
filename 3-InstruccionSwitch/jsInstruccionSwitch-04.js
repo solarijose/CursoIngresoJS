a541f2f1b61adb500387b41684417603f52779bc
Solari Jose Luis <solarijose@gmail.com>
2020-07-27 20:23:29 -0300

    TP

--- 3-InstruccionSwitch/jsInstruccionSwitch-04.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-04.js
@@ -1,8 +1,31 @@
+//SOLARI JOSÉ LUIS
+/*al seleccionar un mes informar.
+si tiene 28 días.
+si tiene 30 días.
+si tiene 31 días.*/
+
 function mostrar()
 {
 	//tomo el mes
 	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
+	
+	switch (mesDelAño)
+	{
+		case "Febrero":
+			alert("Este mes tiene 28 días");
+		break;
+		case "Septiembre":
+		case "Noviembre":
+		case "Abril":
+			alert("Este mes tiene 30 días");
+		break;
+		default:
+			alert("Este mes tiene 31 días");
+	}
+
+	
+	
+//	alert(mesDelAño);
 	
 	
 
