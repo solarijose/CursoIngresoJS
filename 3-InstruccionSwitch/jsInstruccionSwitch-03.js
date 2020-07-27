aede3338bc7c00bee79a1d28b640f6a9394dcfe4
Solari Jose Luis <solarijose@gmail.com>
2020-07-27 20:08:56 -0300

    TP

--- 3-InstruccionSwitch/jsInstruccionSwitch-03.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-03.js
@@ -1,8 +1,23 @@
+//SOLARI JOSÉ LUIS
+/*al seleccionar un mes informar.
+si es Febrero: " Este mes no tiene más de 29 días."
+si NO es Febrero: "Este mes tiene 30 o más días"*/
+
 function mostrar()
 {
 	//tomo el mes
 	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
+	
+	switch(mesDelAño)
+	{
+		case "Febrero":
+		alert("Este mes no tiene más de 29 días.");
+		break;
+		default:
+			alert("Este mes tiene 30 o más días")
+	}
+	
+	//alert(mesDelAño);
 	
 	
 
