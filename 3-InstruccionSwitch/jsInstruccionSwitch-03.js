46edb5829fd8aa85ad6304936e5d716157bacc1b
Solari Jose Luis <solarijose@gmail.com>
2020-07-27 20:09:25 -0300

    TP

--- 3-InstruccionSwitch/jsInstruccionSwitch-03.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-03.js
@@ -11,7 +11,7 @@ function mostrar()
 	switch(mesDelAño)
 	{
 		case "Febrero":
-		alert("Este mes no tiene más de 29 días.");
+			alert("Este mes no tiene más de 29 días.");
 		break;
 		default:
 			alert("Este mes tiene 30 o más días")
