453b559226095364ea52fed9d5d5791e2a7c6add
Solari Jose Luis <solarijose@gmail.com>
2020-07-27 20:02:54 -0300

    TP

--- 3-InstruccionSwitch/jsInstruccionSwitch-02.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-02.js
@@ -1,8 +1,35 @@
+/*al seleccionar un mes informar.
+si estamos en Invierno: "Abrigate que hace frio."
+si aún no llego el Invierno: "Falta para el invierno."
+si ya paso el Invierno: "Ya pasamos el frio, ahora calor!!!."
+ACLARAcIÓN: tomamos a Julio y Agosto como los meses de Invierno.*/
+
 function mostrar()
 {
 	//tomo el mes
 	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
+
+	switch(mesDelAño)
+	{
+		case "Enero":
+		case "Febrero":
+		case "Marzo":
+		case "Abril":
+		case "Mayo":
+		case "Junio":
+		alert("Falta para el invierno.")				
+		break;
+		case "Julio":
+		case "Agosto":
+		alert("Abrigate que hace frio.");
+		break;
+		default:
+			alert("Ya pasamos el frio, ahora calor!!!.")
+
+
+	}
+
+	//alert(mesDelAño);
 
 
 
