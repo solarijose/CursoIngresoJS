b45aabe757f7accfd92e9e87995b325c21991c30
Solari Jose Luis <solarijose@gmail.com>
2020-07-28 17:02:07 -0300

    TP

--- 3-InstruccionSwitch/jsInstruccionSwitch-08.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-08.js
@@ -1,6 +1,26 @@
 function mostrar()
 {
 	var destinoIngresado =txtIdDestino.value;
-	alert(destinoIngresado);
+
+	//Al seleccionar un destino informar si hace FRIO o CALOR en ese destino
+
+	switch(destinoIngresado)
+	{
+		case "Bariloche":
+			alert("Hace Frio");
+			break;
+		case "Cataratas":
+			alert("Hace Calor");
+			break;
+		case "Mar del plata":
+			alert("Hace Calor");
+			break;
+		case "Cordoba":
+			alert("Hace Frio");
+			break;
+
+	}
+
+	//alert(destinoIngresado);
 
 }//FIN DE LA FUNCIÓN
--- 3-InstruccionSwitch/jsInstruccionSwitch-09.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-09.js
@@ -1,6 +1,71 @@
+/*
+PUNTO A)
+una agencia de viajes debe sacar las tarifas de los viajes , 
+se cobra $15.000 por cada estadia como base, 
+se pide el ingreso de una estacion del año 
+y localidad para vacacionar para poder calcular el precio final
+
+PUNTO B)
+en Invierno: 
+bariloche tiene un aumento del 20% cataratas 
+y Cordoba tiene un descuento del 10% 
+Mar del plata tiene un descuento del 20%
+
+PUNTO C)
+en Verano: 
+bariloche tiene un descuento del 20% 
+cataratas y Cordoba tiene un aumento del 10% 
+Mar del plata tiene un aumento del 20%
+
+PUNTO D)
+en Otoño y Primavera: 
+bariloche tiene un aumento del 10% 
+cataratas tiene un aumento del 10% 
+Mar del plata tiene un aumento del 10% y 
+Cordoba tiene el precio sin descuento*/
+
+
 function mostrar()
 {
+
+	var Estadia;
+	var Descuento;
+	var destinoIngresado;
+
+	var Estadia = parseInt(Estadia);
+	Estadia = 15.000;
+
+	var Descuento = parseInt(Descuento);
+
 	var estacionIngresada =txtIdEstacion.value;
-	alert(estacionIngresada);
+	var destinoIngresado =txtIdDestino.value;
+	
+	/*PUNTO A)
+    una agencia de viajes debe sacar las tarifas de los viajes , 
+    se cobra $15.000 por cada estadia como base, 
+    se pide el ingreso de una estacion del año 
+    y localidad para vacacionar para poder calcular el precio final*/
+	switch(estacionIngresada)
+	{
+		case"Invierno":
+			default:
+		//	break;
+		case"Verano":
+			default:
+		//	break;
+		case"Otoño":
+			default:
+		//	break;
+		case"Primavera":
+			alert(estacionIngresada);
+			break;
+	}
+	switch(destinoIngresado)
+	{
+		
+
+	}
+	
+	//alert(estacionIngresada);
 
 }//FIN DE LA FUNCIÓN
