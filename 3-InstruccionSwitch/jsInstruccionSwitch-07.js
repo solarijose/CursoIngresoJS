6af93bf8a10b743a5d2141abd0badf6e51111d05
Solari Jose Luis <solarijose@gmail.com>
2020-07-28 16:16:21 -0300

    TP

--- 3-InstruccionSwitch/jsInstruccionSwitch-07.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-07.js
@@ -1,6 +1,28 @@
+//Al selecionar un destino , indicar el punto cardinal de nuestro pais 
+//en donde se encuentra Norte, Sur, Este u Oeste
+
+
+
 function mostrar()
 {
 	var destinoIngresado =txtIdDestino.value;
-	alert(destinoIngresado);
+	
+	switch(destinoIngresado)
+	{
+		case "Bariloche":
+			alert("Punto cardinal Sur");
+			break;
+		case "Cataratas":
+			alert("Punto cardinal Norte");
+			break;
+		case "Mar del plata":
+			alert("Punto cardinal Este");
+			break;
+		case "Cordoba":
+			alert("Punto Cardinal Oeste");
+			break;
+	}
+
+	//alert(destinoIngresado);
 
 }//FIN DE LA FUNCIÓN
