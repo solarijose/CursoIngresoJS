14d31f540d326720e835f2d840e0fb7ad6f82fff
Solari Jose Luis <solarijose@gmail.com>
2020-07-28 22:01:47 -0300

    TP

--- 3-InstruccionSwitch/jsInstruccionSwitch-09.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-09.js
@@ -8,8 +8,8 @@ y localidad para vacacionar para poder calcular el precio final
 
 PUNTO B)
 en Invierno: 
-bariloche tiene un aumento del 20% cataratas 
-y Cordoba tiene un descuento del 10% 
+bariloche tiene un aumento del 20% 
+cataratas y Cordoba tiene un descuento del 10% 
 Mar del plata tiene un descuento del 20%
 
 PUNTO C)
@@ -28,114 +28,61 @@ Cordoba tiene el precio sin descuento*/
 
 function mostrar()
 {
+	var estacionIngresada;
+	var destino;
+	var descuento;
+	var estadiaPrecio;
+	var precioFinal
 
-	var Estadia;
-	var Descuento;
-	var Destino;
-	var PrecioFinal
-
-	var estacionIngresada =txtIdEstacion.value;
-	Destino =txtIdDestino.value;
-	PrecioFinal = PrecioFinal - Descuento;
+	estacionIngresada =txtIdEstacion.value;
+	destino =txtIdDestino.value;
 	
-	Estadia = 15000;
 
-	/*PUNTO A)
-    una agencia de viajes debe sacar las tarifas de los viajes , 
-    se cobra $15.000 por cada estadia como base, 
-    se pide el ingreso de una estacion del año 
-    y localidad para vacacionar para poder calcular el precio final*/
-	switch(estacionIngresada)
+	switch (estacionIngresada)
 	{
-		case"Invierno":
-		case"Verano":
-		case"Otoño":
-		case"Primavera":
-		break;
+		case "Invierno":
+			switch(destino)
+			{
+				case "Bariloche":
+					descuento = 0.2;
+				break;
+				case "Mar del plata":
+					descuento = -0.2;
+				break;
+				default:	
+					descuento = -0.1;
+			}
+		 break;
+		 case "Verano":
+			switch(destino)
+			{
+				case "Bariloche":
+					descuento = -0.2;
+				break;
+				case "Mar del plata":
+					descuento =  0.2;
+				break;
+				default:
+					descuento =  0.1;
+			}
+			break;
+			default:
+				switch (destino)
+				{
+					case "Cordoba":
+						descuento = 0;
+					break;
+					default:
+						descuento = 0.1;
+				}
+			break;
+
 	}
-    	/*PUNTO B)
-		en Invierno: 
-		bariloche tiene un aumento del 20% cataratas 
-        y Cordoba tiene un descuento del 10% 
-        Mar del plata tiene un descuento del 20%*/
-		switch(estacionIngresada)
-		{
-			case "Invierno":
-				switch(Destino)
-				
-				{
-					case "Bariloche":
-						Descuento = 0.2;
-					break;
-					case "Cordoba":
-						Descuento = 0.1;
-					case "Mar del plata":
-						Descuento = 0.2;
-					break;
-				}
-				/*PUNTO C)
-				en Verano: 
-                bariloche tiene un descuento del 20% 
-                cataratas y Cordoba tiene un aumento del 10% 
-                Mar del plata tiene un aumento del 20%*/
-				break;
-				case "Verano":
-				switch(Destino)
-				{
-					case "Bariloche":
-						Descuento = 0.2;
-					break;
-					case "Mar del plata":
-						Descuento = 0.2;
-					break;
-					case "Cordoba":
-						Descuento = 0.1;
-						default:
-							break;
-					
-				}
-				break;
-				/*PUNTO C)
-				en Verano: 
-				bariloche tiene un descuento del 20% 
-				cataratas y Cordoba tiene un aumento del 10% 
-				Mar del plata tiene un aumento del 20%*/
-				case "Verano":
-				switch(Destino)
-				{
-					case "Bariloche":
-						Descuento = 0.2;
-					case "Mar del plata":
-						Descuento = 0.2;
-					case "Cordoba":
-						Descuento = 0.1;
-						default:
-						break;
-				}
-				break;
-				/*PUNTO D)
-				en Otoño y Primavera: 
-				bariloche tiene un aumento del 10% 
-				cataratas tiene un aumento del 10% 
-				Mar del plata tiene un aumento del 10% y 
-				Cordoba tiene el precio sin descuento*/
-				case "Otoño":
-				case "Primavera":
-					switch(Destino)
-					{
-						case "Bariloche":
-						case "Mar del plata":
-						case "cataratas":
-							Descuento = 0.1;
-						case "Cordoba":
-							Descuento =0;
-							break;
-					}
-					break;
-			
 
-		}  
-		  PrecioFinal = PrecioFinal * Descuento;
-		  alert(PrecioFinal);
+	estadiaPrecio = 15000;
+	precioFinal = estadiaPrecio + estadiaPrecio * descuento;
+	alert(precioFinal);
 
-}//FIN DE LA FUNCIÓN
+}
+
+//FIN DE LA FUNCIÓN
