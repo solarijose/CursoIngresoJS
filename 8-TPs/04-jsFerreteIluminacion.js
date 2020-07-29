036e5801dd0de31cf68036e0be74f58659bab762
Solari Jose Luis <solarijose@gmail.com>
2020-07-29 12:54:38 -0300

    TP

--- 8-TPs/04-jsFerreteIluminacion.js
+++ 8-TPs/04-jsFerreteIluminacion.js
@@ -1,12 +1,16 @@
 /* SOLARI JOSÉ LUIS
 Las lámparas están al mismo precio de $35 pesos final.
 A. Si compra 6 o más lamparitas bajo consumo tiene un descuento del 50%.
+
 B. Si compra 5 lamparitas bajo consumo marca "ArgentinaLuz" se hace un descuento del 40 %
 y si es de otra marca el descuento es del 30%.
+
 C. Si compra 4 lamparitas bajo consumo marca "ArgentinaLuz" o “FelipeLamparas”
 se hace un descuento del 25 % y si es de otra marca el descuento es del 20%.
+
 D. Si compra 3 lamparitas bajo consumo marca "ArgentinaLuz" el descuento es del 15%,
 si es “FelipeLamparas” se hace un descuento del 10 % y si es de otra marca un 5%.
+
 E. Si el importe final con descuento suma más de $120 se debe sumar un 10%
 de ingresos brutos en informar del impuesto con el siguiente mensaje: 
 ”IIBB Usted pago X”, siendo X el impuesto que se pagó*/
@@ -115,7 +119,7 @@ function CalcularPrecio()
 var cantidadLamparas;
 var precioLampras;
 var descuento;
-var precioDescuento;
+var descuento;
 var importeFinal;
 var marcaLamparas;
 var impuestos;
