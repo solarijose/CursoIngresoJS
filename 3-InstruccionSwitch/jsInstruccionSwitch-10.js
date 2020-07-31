a10be2bed4a8f63740a4f40e5d036466d1531d34
Solari Jose Luis <solarijose@gmail.com>
2020-07-31 16:14:24 -0300

    TP

--- 3-InstruccionSwitch/jsInstruccionSwitch-10.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-10.js
@@ -1,41 +1,71 @@
-/*
+/*SOLARI JOSÉ LUIS
 una agencia de viajes nos piden informar si hacemos viajes a lugares 
-según la estación del año estemos, informar si "Se viaja" o "No se viaja" a ese lugar
+según la estación del año estemos, informar si "Se viaje" o "No se viaje" a ese lugar
 
-en Invierno: Solo Bariloche informa "se viaja" los demas destinos "No se viaja"
-en Verano: Se viaja a Mar del plata y Cataratas solamente
-en Otoño: Se viaja a todos los destinos.
-primavera: solo no se viaja a Bariloche*/
+en Invierno: Solo Bariloche informa "se viaje" 
+los demas destinos "No se viaje"
+en Verano: Se viaje a Mar del plata y Cataratas solamente
+en Otoño: Se viaje a todos los destinos.
+primavera: solo no se viaje a Bariloche*/
 
 
 function mostrar()
 {
 
 	var estacionIngresada;
-	var destino;
-	var seViaja;
-	var noViaja;
+	var destinoIngresado;
+	var viaje;
 
 
 
 	var estacionIngresada =txtIdEstacion.value;
+	var destinoIngresado = txtIdDestino.value;
 	
 
 	switch(estacionIngresada)
 	{
+		//en Invierno: Solo Bariloche informa "se viaje" 
 		case "Invierno":
-		switch(destino)
+		switch(destinoIngresado)		
 		{
-			case "Bariloche"
-				alert("Se viaja");
-				break;
-			case ""
-
+			case "Bariloche":
+				viaje = "Se viaja";
+			break;
+			default:
+				viaje = "No se viaja";
+			break;
 		}
+		break;
+		//Verano: Se viaje a Mar del plata y Cataratas solamente
+		case "Verano":
+		switch(destinoIngresado)
+		{
+			case "Mar del plata":
+			case "Cataratas":
+				viaje = "Se viaja";
+				break;
+			default:
+				viaje = "No se viaja";
+				break;
+		}
+		break;
+		case "Otoño"://Otoño: Se viaje a todos los destinos.	
+			viaje= "Se viaja";
+			break;
+		case "Primavera"://primavera: solo no se viaje a Bariloche
+		switch(destinoIngresado)
+		{
+			case "Bariloche":
+				viaje= "No se viaja";
+			break;
+			default:
+				viaje= "Se viaja";
+				break;
+		}
+		break;
 
 	}
-
-
+	alert(viaje);
 
 
 
