235614a9774b31400119d726d450a83793f0cd09
Solari Jose Luis <solarijose@gmail.com>
2020-08-08 19:15:18 -0300

    TP

--- 9-Parciales/2020_1Cuatri_recu_fuentes/dos.js
+++ 9-Parciales/2020_1Cuatri_recu_fuentes/dos.js
@@ -1,4 +1,134 @@
+/*SOLARI JOSÉ LUIS
+Realizar el algoritmo que permita ingresar los datos de una compra de productos de la construccion, 
+hasta que el cliente quiera:
+Tipo validar("arena";"cal";"cemento")
+Cantidad de bolsas,
+Precio por bolsa (más de cero ),
+
+al terminar la compra el cliente accede a un descuento segun las bolsas en total
+Si compro más de 10 bolsas en total tenes 15% de descuento sobre el total a pagar.
+Si compro más de 30 bolsas en total tenes 25% de descuento sobre el total a pagar.
+
+
+a) El importe total a pagar , bruto sin descuento y...
+b) el importe total a pagar con descuento(solo si corresponde)
+d) Informar el tipo ("arena";"cal";"cemento") con mas cantidad de bolsas en el total de la compra.
+f) El tipo mas caro
+
+*/
+/*
 function mostrar()
 {
-  alert("dos");
+  
+  var Tipo;
+  var cantidadDeBolsas;
+  var precioPorBolsa;
+  var respuesta;
+
+  respuesta = "s";
+  while(respuesta == "s")
+
+  {
+    
+    respuesta = prompt("seguir ?")
+
+  }
+
+}*/
+
+function mostrar(){
+
+  var tipo;
+  var cantidadDeBolsas;
+  var precioPorBolsa;
+  var respuesta = "si";
+  var acumuladorBolas = 0;
+  var total = 0;
+  var acumArena = 0;
+  var acumCemento = 0;
+  var acumCal = 0;
+
+  while(respuesta == "si")
+  {
+     
+      tipo = prompt("Ingrese el tipo: Arena,Cal o Cemento ");
+
+      while(tipo != "arena" && tipo != "cal" && tipo != "cemento") 
+      {
+          alert("el tipo debe ser arena,cal o cemento");
+          tipo = prompt("Ingrese el tipo: ");
+      }
+
+      cantidadDeBolsas = parseInt(prompt("Ingrese cantidad de bolsas:"));
+      
+      while(cantidadDeBolsas <1)
+      {
+         alert("Debe ingresar una cantidad mayor a 0");
+         cantidadDeBolsas = parseInt(prompt("Ingrese cantidad de bolsas:"));
+      }
+
+      acumuladorBolas = acumuladorBolas + cantidadDeBolsas;
+
+      switch(tipo){
+          case 'arena':
+            acumArena += cantidadDeBolsas;
+          break;
+
+          case 'cal':
+            acumCal += cantidadDeBolsas;
+          break;
+
+          case 'cemento':
+            acumCemento += cantidadDeBolsas;
+          break;
+      }
+
+      precioPorBolsa = parseInt(prompt("Ingrese el precio por bolsa:"));
+
+      while(precioPorBolsa < 1)
+      {
+         alert("el valor debe ser mayor a 0");
+         precioPorBolsa = parseInt(prompt("Ingrese el precio por bolsa:"));
+      }
+      
+      total = total + (precioPorBolsa * cantidadDeBolsas); 
+  
+     respuesta = prompt("Seguir ? si/no: ");
+  }
+
+  var descuento;
+
+  if(acumuladorBolas > 10 && acumuladorBolas <= 30){
+
+      //calcular 15% de descuento sobre el total
+      descuento = (15*total)/100;
+      alert("El descuento del 15% es de "+ descuento+" sobre el total de "+total);
+  }
+
+  if(acumuladorBolas > 30){
+
+      //calcular 25% de descuento sobre el total
+      descuento = (25*total)/100;
+      alert("El descuento del 25% es de "+ descuento+" sobre el total de "+total);
+  }
+
+  alert("El total bruto a pagar es de $: "+total);
+
+  if(acumArena > acumCal && acumArena > acumCemento)
+  {
+    alert("Arena vendio mas");
+  }
+  if(acumCal > acumArena && acumCal > acumCemento)
+  {
+    alert("cal vendio mas");
+  }
+  if(acumCemento > acumArena && acumCemento > acumCal)
+  {
+    alert("cemento vendio mas");
+  }
+ 
 }
+
+
+
+
--- 9-Parciales/2020_1Cuatri_recu_fuentes/uno.js
+++ 9-Parciales/2020_1Cuatri_recu_fuentes/uno.js
@@ -1,5 +1,56 @@
+/*
+Debemos realizar la carga de una compra de 5(cinco) productos de prevención de contagio,
+de cada una debo obtener los siguientes datos:
+el tipo de producto (validar "barbijo" , "jabón" o "alcohol") ,
+el precio (validar entre 100 y 300),
+la cantidad de unidades (no puede ser 0 o negativo y no debe superar las 1000 unidades),
+la Marca y el fabricante.
 
+Se debe Informar al usuario lo siguiente:
+a) Del más caro de los jabones, la cantidad de unidades y el fabricante
+b) Del tipo de producto con más unidades en total de la compra, el promedio por compra
+c) Cuántas unidades de Barbijos se compraron en total
+
+*/
 function mostrar()
 {
-	alert("uno");
+	var tipo;
+	var precio;
+	var cantidadDeUnidades;
+	var marca;
+	var fabricante;
+	var tempPrecio = 0;
+	var jabonMasCaroFabricante = 0;
+	var jabonMasCaroUnidades = 0;
+
+	for(var i=0;i<2;i++){
+
+		tipo = prompt("Ingrese tipo de producto: ");
+		while(tipo != "barbijo" && tipo != "jabon" && tipo != "alcohol"){
+			tipo = prompt("Ingrese tipo de producto: ");
+		}
+
+		var precio = parseInt(prompt("Ingrese precio: "));
+		while(precio < 100 || precio > 300){
+			precio = parseInt(prompt("Ingrese precio: "));
+		}
+		
+		cantidadDeUnidades = parseInt(prompt("Ingrese cant unidades: "));
+		while(cantidadDeUnidades < 0 || cantidadDeUnidades > 1000){
+			cantidadDeUnidades = parseInt(prompt("Ingrese cant unidades: "));
+		}
+
+		marca = prompt("Ingrese marca: ");
+		fabricante = prompt("Ingrese fabricante: ");
+
+		if(tipo == "jabon" && precio > tempPrecio){
+			tempPrecio = precio;
+			jabonMasCaroFabricante = fabricante;
+			jabonMasCaroUnidades = cantidadDeUnidades
+		}
+
+	}
+
+ alert("fabricante"+ jabonMasCaroFabricante);
+ alert("unidades"+ jabonMasCaroUnidades);
 }
